df479e76bf5cad54b1e12daae990a6b958af6a2c
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-04-13 20:08:52 +0200

    feat: register functionality
    - added selector for user to choose whether to register as traveler or host
    - navigate user to login page after registration is successful

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        slideDown: "slideDown 0.5s ease-out",
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { opacity: 0, transform: 'translateY(-30px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
       fontFamily: {
         sans: ["Quicksand", "sans-serif"],
         alt: ["Lato", "sans-serif"],
