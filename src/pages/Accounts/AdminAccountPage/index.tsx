9a7ff0ec2cf50986e6df179ae61c4aebee50ee54
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-04-17 20:09:54 +0200

    feat: added Venues overview
    added venue overview to admin dashboard

--- src/pages/Accounts/AdminAccountPage/index.tsx
+++ src/pages/Accounts/AdminAccountPage/index.tsx
@@ -1,8 +1,10 @@
 import { useFetchVenuesByProfile } from '../../../hooks/useFetchVenuesByProfile.ts';
 import AdminStats from '../../../components/accounts/AdminStats.tsx';
+import VenueOverview from '../../../components/accounts/VenueOverview.tsx';
 
 export default function AdminPage() {
     const { venues, isLoading, error } = useFetchVenuesByProfile();
+
     if (isLoading) return <p>Loading profile...</p>;
     if (error) return <p>Error: {error}</p>;
 
@@ -10,7 +12,7 @@ export default function AdminPage() {
         <>
             <h1 className="justify-self-center text-3xl font-bold mb-4">Admin Dashboard</h1>
             <AdminStats venues={venues} />
+            <VenueOverview venues={venues} />
         </>
-
     );
 }
